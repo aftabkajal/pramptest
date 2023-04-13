4e0684572f1eace93808e42281f57e9d13e840b2
Aftab Uddin Kajal <kajal.alpha@gmail.com>
2023-04-13 20:52:41 +0600

    Decrypter class added

---
+++ src/Decrypter.cs
@@ -0,0 +1,25 @@
+﻿namespace Pramptests
+{
+    public class Decrypter
+    {
+        public static string Decrypt(string word)
+        {
+            string decryptedWord = "";
+            int oldValue = 1, newValue;
+            int valueOfA = (int)'a';
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                newValue = (int)(word[i]) - oldValue;
+                oldValue = (int)(word[i]);
+
+                while (newValue < valueOfA)
+                {
+                    newValue += 26;
+                }
+                decryptedWord = decryptedWord + (char)newValue;
+            }
+            return decryptedWord;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Pramptests
+{
+    class Solution
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decrypter.Decrypt("dnotq"));
+            Console.WriteLine(Decrypter.Decrypt("flgxswdliefy"));
+        }
+    }
+}
